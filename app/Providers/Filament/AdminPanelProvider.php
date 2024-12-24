97aba67768385111c6f87b63765b5a3b7f03f04f
mnurullahsaglam <nurullahsl87@gmail.com>
2024-12-25 02:20:58 +0300

    v0.2.6

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers\Filament;
 
+use App\Filament\Clusters\Library\Resources\BookResource\Widgets\BooksOverview;
 use App\Filament\Pages\Login;
 use Filament\Http\Middleware\Authenticate;
 use Filament\Http\Middleware\AuthenticateSession;
@@ -47,7 +48,8 @@ class AdminPanelProvider extends PanelProvider
             ->widgets([
                 AccountWidget::class,
                 FilamentInfoWidget::class,
-                PanOverview::class
+                BooksOverview::class,
+                PanOverview::class,
             ])
             ->plugins([
                 SpotlightPlugin::make(),
