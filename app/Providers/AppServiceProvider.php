4079957909637bf6bc8864fbb80581131b180aa6
mnurullahsaglam <nurullahsl87@gmail.com>
2025-01-06 11:47:54 +0300

    v0.2.8

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -37,36 +37,36 @@ class AppServiceProvider extends ServiceProvider
             return $user->email === config('app.admin_email');
         });
 
-        CreateAction::configureUsing(function (CreateAction $action) {
+        CreateAction::configureUsing(function (CreateAction $action): void {
             $action->icon('heroicon-o-plus');
         });
 
-        ImportAction::configureUsing(function (ImportAction $action) {
+        ImportAction::configureUsing(function (ImportAction $action): void {
             $action
                 ->color('info')
                 ->icon('heroicon-o-document-arrow-up')
                 ->translateLabel();
         });
 
-        ExportAction::configureUsing(function (ExportAction $action) {
+        ExportAction::configureUsing(function (ExportAction $action): void {
             $action
                 ->color('primary')
                 ->icon('heroicon-o-document-arrow-down')
                 ->translateLabel();
         });
 
-        ExportBulkAction::configureUsing(function (ExportBulkAction $action) {
+        ExportBulkAction::configureUsing(function (ExportBulkAction $action): void {
             $action
                 ->color('primary')
                 ->icon('heroicon-o-document-arrow-down')
                 ->translateLabel();
         });
 
-        Field::configureUsing(function (Field $field) {
+        Field::configureUsing(function (Field $field): void {
             $field->translateLabel();
         });
 
-        Column::configureUsing(function (Column $column) {
+        Column::configureUsing(function (Column $column): void {
             $column->translateLabel();
         });
     }
